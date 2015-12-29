71fa8a3baff2cc82e8a64ac286d4e02fbdc10c63
Dylan <invision33@gmail.com>
2015-12-29 03:57:15 -0600

    add spring and rating

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -4,7 +4,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
 	setup do 
 		#create! raise an error if it doesn't work 
 		Book.create!(title: 'Pragmatic Programmer', rating: 5)
-		Book.create!(title: "Ender's game", rating: 5)
+		Book.create!(title: "Ender's game", rating: 4)
 	end 
 	
   test 'listing books' do 
@@ -15,5 +15,14 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
 
   	assert_equal Book.count, JSON.parse(response.body).size 
   	#parse returns an array, calling size on it 
+  end 
+
+  test 'lists top rated books' do 
+    get '/books?rating=5'
+
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    assert_equal 1, JSON.parse(response.body).size 
   end 
 end
